4f18d9cff2dc827cbef5c77c2487eef8e328886f
leoanrdo77pk <suporte.purofut@gmail.com>
2025-07-07 23:18:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const axios = require('axios');
 
+module.exports = async (req, res) => {
+  try {
+    // Solicita o conteúdo do site externo
+    const response = await axios.get('https://futebol7k.com');
+    
+    // Envia o conteúdo de volta para o navegador
+    res.status(200).send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao carregar conteúdo');
+  }
+};
