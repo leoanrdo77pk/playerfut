eba245266452361cf43f2d7a04a5bf3219678607
leoanrdo77pk <suporte.purofut@gmail.com>
2025-07-08 00:25:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,44 @@ module.exports = async (req, res) => {
         .replace(/action="\/([^"]+)"/g, 'action="/$1"')
         .replace(/<base[^>]*>/gi, '');
 
+      // Código do footer fixo
+      const footerHtml = `
+      <div id="custom-footer">
+        <a href="https://8xbet86.com/" target="_blank">
+          <img src="https://i.imgur.com/Fen20UR.gif" alt="Banner de Anúncio" />
+        </a>
+      </div>
+      <style>
+        #custom-footer {
+          position: fixed;
+          bottom: 0;
+          width: 100%;
+          z-index: 9999;
+          background: transparent;
+          text-align: center;
+          padding: 10px;
+        }
+        
+        #custom-footer img {
+          width: 100%;
+          max-height: 100px;
+          object-fit: contain;
+          cursor: pointer;
+        }
+
+        body {
+          padding-bottom: 120px;
+        }
+      </style>
+      </body>`;
+
+      // Injeta o footer no final da página
+      const finalHtml = data.replace('</body>', footerHtml);
 
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Content-Type', resp.headers['content-type'] || 'text/html');
       res.statusCode = 200;
-      res.end(data);
+      res.end(finalHtml);
     });
   }).on("error", (err) => {
     console.error("Erro:", err.message);
