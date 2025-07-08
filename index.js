3bcf9e4fe244aa7935286bf445a14a2ac927b09f
leoanrdo77pk <suporte.purofut@gmail.com>
2025-07-07 23:33:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+const https = require('https');
+
+module.exports = async (req, res) => {
+  const targetUrl = 'https://futebol7k.com' + req.url;
+
+  https.get(targetUrl, { headers: {
+    'User-Agent': req.headers['user-agent'] || 'Mozilla/5.0',
+    'Referer': 'https://futebol7k.com'
+  }}, (resp) => {
+    let data = '';
+
+    resp.on('data', chunk => data += chunk);
+    resp.on('end', () => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Content-Type', resp.headers['content-type'] || 'text/html');
+      res.statusCode = 200;
+      res.end(data);
+    });
+  }).on("error", (err) => {
+    console.error("Erro:", err.message);
+    res.statusCode = 500;
+    res.end("Erro ao carregar conteúdo");
+  });
+};
