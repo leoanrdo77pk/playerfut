0a1f6195c2bb11a3adf8867c8272f2a462989726
leoanrdo77pk <suporte.purofut@gmail.com>
2025-07-07 23:38:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-
 const https = require('https');
 
 module.exports = async (req, res) => {
-  const targetUrl = 'https://futebol7k.com' + req.url;
+  const path = req.url === '/' ? '' : req.url;
+  const targetUrl = 'https://futebol7k.com' + path;
 
-  https.get(targetUrl, { headers: {
-    'User-Agent': req.headers['user-agent'] || 'Mozilla/5.0',
-    'Referer': 'https://futebol7k.com'
-  }}, (resp) => {
+  https.get(targetUrl, {
+    headers: {
+      'User-Agent': req.headers['user-agent'] || 'Mozilla/5.0',
+      'Referer': 'https://futebol7k.com',
+    }
+  }, (resp) => {
     let data = '';
 
     resp.on('data', chunk => data += chunk);
     resp.on('end', () => {
+      // Reescreve todos os links internos para apontar pro seu domínio
+      data = data.replace(/href="\/([^"]+)"/g, 'href="/$1"');
+      data = data.replace(/href='\/([^']+)'/g, "href='/$1'");
+      data = data.replace(/action="\/([^"]+)"/g, 'action="/$1"');
+      data = data.replace(/<base[^>]*>/g, ''); // remove <base> se tiver
+
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Content-Type', resp.headers['content-type'] || 'text/html');
       res.statusCode = 200;
