57286638b991cfb0179b2c9c433710db1aeb491b
leoanrdo77pk <suporte.purofut@gmail.com>
2025-07-08 00:19:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,58 +22,13 @@ module.exports = async (req, res) => {
         .replace(/action="\/([^"]+)"/g, 'action="/$1"')
         .replace(/<base[^>]*>/gi, '');
 
-      // Script do banner catfish
-      const bannerScript = `
-<script>
-(function () {
-  const url = "https://8xbet86.com/";
-  const footerImgDesktop = "https://i.imgur.com/Fen20UR.gif";
-  const footerImgMobile = "https://i.imgur.com/JtIJ2hM.gif";
-
-  const style = document.createElement("style");
-  style.textContent = \`
-    #catfish-footer {
-      position: fixed; bottom: 0; width: 100%;
-      z-index: 9997; background: transparent;
-      text-align: center;
-    }
-    #catfish-footer img {
-      width: 100%; max-height: 100px;
-      object-fit: contain;
-      cursor: pointer;
-    }
-    body { padding-bottom: 100px; }
-  \`;
-  document.head.appendChild(style);
-
-  const footer = document.createElement("div");
-  footer.id = "catfish-footer";
-
-  const footerLink = document.createElement("a");
-  footerLink.href = url;
-  footerLink.target = "_blank";
-
-  const footerImgEl = document.createElement("img");
-  footerImgEl.src = window.innerWidth <= 768 ? footerImgMobile : footerImgDesktop;
-
-  footerLink.appendChild(footerImgEl);
-  footer.appendChild(footerLink);
-
-  window.addEventListener("DOMContentLoaded", () => {
-    document.body.appendChild(footer);
-  });
-})();
-</script>
-</body>
-`;
-
-      // Injeta o banner antes da tag </body>
-      const finalHtml = data.replace('</body>', bannerScript);
+      // Substitui o código do banner para carregar o script externo
+      data = data.replace('</body>', '<script src="/banner.js"></script></body>');
 
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Content-Type', resp.headers['content-type'] || 'text/html');
       res.statusCode = 200;
-      res.end(finalHtml);
+      res.end(data);
     });
   }).on("error", (err) => {
     console.error("Erro:", err.message);
