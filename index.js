672dbedb22240a56d91e4baff071fdcc1a2d176b
Tub play <suporte.purofut@gmail.com>
2025-08-08 18:57:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const https = require('https');
 module.exports = async (req, res) => {
   try {
     const path = req.url === '/' ? '' : req.url;
-    const targetUrl = 'https://futebol7k.com/' + path;
+    const targetUrl = 'https://futemax.gratis/' + path;
 
     https.get(targetUrl, {
       headers: {
         'User-Agent': req.headers['user-agent'] || 'Mozilla/5.0',
-        'Referer': 'https://futebol7k.com/',
+        'Referer': 'https://futemax.gratis/',
       }
     }, (resp) => {
       let data = '';
