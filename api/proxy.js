4994c9af1f6d7c9019275d3b4436deecde62eb05
Tub play <suporte.purofut@gmail.com>
2025-08-12 15:01:36 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -119,8 +119,8 @@ module.exports = async (req, res) => {
       if (html.includes('</body>')) {
         html = html.replace('</body>', `
 <div id="custom-footer">
-  <a href="https://8xbet86.com/" target="_blank">
-    <img src="https://i.imgur.com/Fen20UR.gif" style="width:100%;max-height:100px;object-fit:contain;cursor:pointer;" alt="Banner" />
+  <a href="" target="_blank">
+    <img src="https://i.ibb.co/XFmnzY7/Captura-de-tela-2024-11-11-112206.png" style="width:100%;max-height:100px;object-fit:contain;cursor:pointer;" alt="Banner" />
   </a>
 </div>
 <style>
@@ -137,8 +137,8 @@ module.exports = async (req, res) => {
       } else {
         html += `
 <div id="custom-footer">
-  <a href="https://8xbet86.com/" target="_blank">
-    <img src="https://i.imgur.com/Fen20UR.gif" style="width:100%;max-height:100px;object-fit:contain;cursor:pointer;" alt="Banner" />
+  <a href="" target="_blank">
+    <img src="https://i.ibb.co/XFmnzY7/Captura-de-tela-2024-11-11-112206.png" style="width:100%;max-height:100px;object-fit:contain;cursor:pointer;" alt="Banner" />
   </a>
 </div>
 <style>
