47f65798a31488cbe542e16d09e940a64e69da4d
Tub play <suporte.purofut@gmail.com>
2025-11-27 23:13:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const https = require('https');
 module.exports = async (req, res) => {
   try {
     const path = req.url === '/' ? '' : req.url;
+
+    // Página principal fixa
     const targetUrl = 'https://sinalpublico.vercel.app/menusmarttv.html' + path;
 
     https.get(targetUrl, {
@@ -18,40 +20,51 @@ module.exports = async (req, res) => {
 
       resp.on('end', () => {
         try {
+          // liberar frame
           const headers = { ...resp.headers };
           delete headers['x-frame-options'];
           delete headers['content-security-policy'];
 
-          // -------------------------
-          // CORREÇÃO DAS REGRAS DE REWRITE
-          // -------------------------
+          // ----------------------------
+          // REGRAS DE REWRITE CORRETAS
+          // ----------------------------
           data = data
-            .replace(/https:\/\/futebol7k\.com\//g, '/')
 
-            // Corrigido: regex válida agora
-            .replace(/src="https:\/\/sinalpublico\.vercel\.app\/menusmarttv\.html\/([^"]+)"/g, 'src="/$1"')
+            // remove URLs absolutas do domínio original
+            .replace(/https:\/\/sinalpublico\.vercel\.app\/menusmarttv\.html\/([^"']+)/g, '/$1')
+            .replace(/https:\/\/sinalpublico\.vercel\.app\/([^"']+)/g, '/$1')
 
-            .replace(/src='https:\/\/futebol7k\.com\/([^']+)'/g, "src='/$1'")
-            .replace(/href="https:\/\/futebol7k\.com\/([^"]+)"/g, 'href="/$1"')
-            .replace(/href='https:\/\/futebol7k\.com\/([^']+)'/g, "href='/$1'")
-            .replace(/action="https:\/\/futebol7k\.com\/([^"]+)"/g, 'action="/$1"')
-            .replace(/url\(["']?https:\/\/futebol7k\.com\/(.*?)["']?\)/g, 'url("/$1")')
-            .replace(/<iframe([^>]*)src=["']https:\/\/futebol7k\.com\/([^"']+)["']/g, '<iframe$1src="/$2"')
+            // src=""
+            .replace(/src=["']https:\/\/sinalpublico\.vercel\.app\/([^"']+)["']/g, 'src="/$1"')
+
+            // href=""
+            .replace(/href=["']https:\/\/sinalpublico\.vercel\.app\/([^"']+)["']/g, 'href="/$1"')
+
+            // action=""
+            .replace(/action=["']https:\/\/sinalpublico\.vercel\.app\/([^"']+)["']/g, 'action="/$1"')
+
+            // url(...)
+            .replace(/url\(["']?https:\/\/sinalpublico\.vercel\.app\/(.*?)["']?\)/g, 'url("/$1")')
+
+            // iframe
+            .replace(
+              /<iframe([^>]*)src=["']https:\/\/sinalpublico\.vercel\.app\/([^"']+)["']/g,
+              '<iframe$1src="/$2"'
+            )
+
+            // remove base
             .replace(/<base[^>]*>/gi, '');
 
-          // Alterações visuais
+          // editar título e remover favicon
           data = data
-            .replace(/<title>[^<]*<\/title>/, '<title>Futebol ao Vivo</title>')
+            .replace(/<title>[^<]*<\/title>/, '<title>Futebol Ao Vivo</title>')
             .replace(/<link[^>]*rel=["']icon["'][^>]*>/gi, '')
-            .replace(
-              /<head>/i,
-              `<head>\n<meta name="ppck-ver" content="82de547bce4b26acfb7d424fc45ca87d" />`
-            );
+            .replace(/<head>/i, `<head>\n<meta name="ppck-ver" content="82de547bce4b26acfb7d424fc45ca87d" />`);
 
-          // Remover scripts
+          // remover scripts
           data = data.replace(/<script[^>]*>[\s\S]*?<\/script>/gi, '');
 
-          // Banner
+          // banner
           const banner = `
 <div id="custom-footer">
 <script type="text/javascript" src="//static.scptp9.com/mnpw3.js"></script>
@@ -68,6 +81,7 @@ mnpw.add('https://t.mbsrv2.com/273605/7566?popUnder=true&aff_sub5=SF_006OG000004
             ? data.replace('</body>', `${banner}</body>`)
             : data + banner;
 
+          // resposta final
           res.writeHead(200, {
             ...headers,
             'Access-Control-Allow-Origin': '*',
